d84ce4fd143cbc7bb2a5f8ebde33157db6f81715
Ricky <rickychouw@gmail.com>
2024-12-22 20:15:33 +0800

    test

---
+++ sfml-test/main.cpp
@@ -0,0 +1,144 @@
+#include <SFML/Graphics.hpp>
+
+class TileMap : public sf::Drawable, public sf::Transformable
+{
+public:
+
+    bool load(const std::string& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height)
+    {
+        // load the tileset texture
+        if (!m_tileset.loadFromFile(tileset))
+            return false;
+
+        // resize the vertex array to fit the level size
+        m_vertices.setPrimitiveType(sf::Quads);
+        m_vertices.resize(width * height * 4);
+
+        // populate the vertex array, with one quad per tile
+        for (unsigned int i = 0; i < width; ++i)
+            for (unsigned int j = 0; j < height; ++j)
+            {
+                // get the current tile number
+                int tileNumber = tiles[i + j * width];
+
+                // find its position in the tileset texture
+                int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
+                int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
+
+                // get a pointer to the current tile's quad
+                sf::Vertex* quad = &m_vertices[(i + j * width) * 4];
+
+                // define its 4 corners
+                quad[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
+                quad[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
+                quad[2].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
+                quad[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
+
+                // define its 4 texture coordinates
+                quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
+                quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
+                quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
+                quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
+            }
+
+        return true;
+    }
+
+private:
+
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+    {
+        // apply the transform
+        states.transform *= getTransform();
+
+        // apply the tileset texture
+        states.texture = &m_tileset;
+
+        // draw the vertex array
+        target.draw(m_vertices, states);
+    }
+
+    sf::VertexArray m_vertices;
+    sf::Texture m_tileset;
+};
+
+int main()
+{
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(960, 720), "My window");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        // define the level with an array of tile indices
+        const int level[] =
+        {
+            0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0,
+            1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3,
+            0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0,
+            0, 1, 1, 0, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 0, 0,
+            0, 0, 1, 0, 3, 0, 2, 2, 0, 0, 1, 1, 1, 1, 2, 0,
+            2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1,
+            0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1,
+        };
+
+        // create the tilemap from the level definition
+        TileMap map;
+        if (!map.load("840pxUNO.png", sf::Vector2u(60, 90), level, 16, 8))
+            return -1;
+        window.draw(map);
+        // // draw everything here...
+        // sf::Texture texture;
+        // if (!texture.loadFromFile("840pxUNO.png", sf::IntRect(0, 0, 60, 90)));
+        // {
+        //     // error...
+        // }
+        // sf::Sprite sprite;
+        // sprite.setTexture(texture);
+        // window.draw(sprite);
+
+        // sf::Text text;
+
+        // // select the font
+        // sf::Font font;
+        // if (!font.loadFromFile("ARIAL.TTF"))
+        // {
+        //     // error...
+        // }
+        // text.setFont(font); // font is a sf::Font
+
+        // // set the string to display
+        // text.setString("Hello world");
+
+        // // set the character size
+        // text.setCharacterSize(24); // in pixels, not points!
+
+        // // set the color
+        // text.setFillColor(sf::Color::Red);
+
+        // // set the text style
+        // text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+        // text.setPosition(sf::Vector2f(60.f, 90.f));
+
+        // // inside the main loop, between window.clear() and window.display()
+        // window.draw(text);
+
+        // end the current frame
+        window.display();
+    }
+
+    return 0;
+}
